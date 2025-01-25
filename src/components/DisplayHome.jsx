ca84d4df6b66e6d88f0d5c8f0834f556c4c3716e
Isaac Oteng <wisebornjnr@gmail..com>
2025-01-25 00:03:13 +0000

    second commit

--- src/components/DisplayHome.jsx
+++ src/components/DisplayHome.jsx
@@ -13,13 +13,13 @@ const DisplayHome = () => {
     <>
     <Navbar />
 
-    <div className='mb-4'>
+    <div className='m b-4'>
     <h1 className='my-5 font-bold text-2x1'>Featured Charts</h1>
     <div className='flex overflow-auto'>
       {albumsData.map((item,index) => (<AlbumItem key={index} name={item.name} desc={item.desc} id={item.id} image={item.image} />) )}
     </div>
     </div>
-
+    
     <div className='mb-4'>
     <h1 className='my-5 font-bold text-2x1'>Today's Biggest Hits</h1>
     <div className='flex overflow-auto'>
